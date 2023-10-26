75623a760f90a160945865556048520cf93769f2
João Henrique <jh36750@gmail.com>
2023-10-26 07:22:03 -0300

    API Authentication controller

--- EcommerceApplication.API/Models/AuthenticationModel.cs
+++ EcommerceApplication.API/Models/AuthenticationModel.cs
@@ -1,6 +1,21 @@
-﻿namespace EcommerceApplication.API.Models
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace EcommerceApplication.API.Models
 {
-    public class Class
+    public class AuthenticationModel
     {
+        public string identifierUser { get; set; }
+        public string pass { get; set; }
+        public long? idCompany { get; set; }
+
+        public void Validate()
+        {
+            var validator = new List<string>(); //AuthenticationValidator();
+
+            var resultadoValidacao = validator;//.Validate(this);
+
+            //if (!resultadoValidacao.IsValid)
+            //    throw new Exception(string.Join(";", resultadoValidacao.Errors.Select(e => e.ErrorMessage).ToList()));
+        }
     }
 }
---
+++ EcommerceApplication.Service/Contracts/IAuthenticationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceApplication.Service.Contracts
+{
+    public interface IAuthenticationService
+    {
+        Task<bool> Authenticate(string identifierUser, string pass, long? idCompany);
+    }
+}
